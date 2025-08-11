96679232a5f23a1a6b380f68f4a6fe98b5cb6e3d
Marius Meier <marius.meier24@gmail.com>
2025-08-11 17:50:04 +0300

    adds linking to profile page

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -6,17 +6,23 @@ interface ProjectTileProps {
 }
 
 const ProjectTile = ({ image, title, description, link = "#" } : ProjectTileProps) => {
+	const handleTileClick = () => {
+		if (link && link !== "#") {
+			window.open(link, '_blank');
+		}
+	};
 	return (
-		<div className="max-w-sm overflow-hidden rounded-xl bg-white shadow-md hover:shadow-lg hover:-translate-y-1 transition-all duration-300 cursor-pointer">
+		<div className="max-w-sm overflow-hidden rounded-xl bg-white shadow-md hover:shadow-lg hover:-translate-y-1 transition-all duration-300 cursor-pointer"
+		onClick={handleTileClick}>
 			<img
 				className="h-48 w-full object-cover"
 				src={image}
 				alt={title}
 			/>
 			<div className="p-8">
-			<a href={link} className="mt-1 block text-lg leading-tight font-medium text-black">
+			<h3 className="mt-1 block text-lg leading-tight font-medium text-black">
 				{title}
-			</a>
+			</h3>
 			<p className="mt-2 text-gray-500">
 				{description}
 			</p>
