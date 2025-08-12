9cbeaf943a3431cf12cde5b26237f240a69e0af0
Marius Meier <marius.meier24@gmail.com>
2025-08-12 12:14:32 +0300

    adds animated button

--- src/components/SplitScreen.tsx
+++ src/components/SplitScreen.tsx
@@ -1,4 +1,5 @@
 import me from '../assets/me.jpeg';
+import NavLink from './NavLink';
 
 const SplitScreen = () => {
 	const descriptiveText = "I am an aspiring software developer with main interest in backend/api development. Open minded and always up to learn about new tech and tools"
@@ -11,6 +12,11 @@ const SplitScreen = () => {
 						<h1 className="text-4xl font-bold text-gray-900 dark:text-white pr-8">{"<Hi there, nice to meet you>"}</h1>
 						<p className="mt-4 text-lg text-gray-600 dark:text-gray-300 pr-8">{descriptiveText}</p>
 						<p className="mt-4 text-lg text-gray-600 dark:text-gray-300 pr-8">Check out what I have been working on recently</p>
+						<div className="bg-neutral-50 dark:bg-gray-800 rounded-full p-3 animate-bounce mt-8 mx-auto cursor-pointer w-fit">
+						<svg className="size-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+						</svg>
+						</div>
 					</div>
 				</div>
 				
