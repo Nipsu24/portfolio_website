b9f023d58e19dc580e9ee69fe260b4f4e5c18850
Marius Meier <marius.meier24@gmail.com>
2025-08-12 10:51:10 +0300

    adds dark mode optimisation for several components

--- src/components/SplitScreen.tsx
+++ src/components/SplitScreen.tsx
@@ -3,9 +3,9 @@ import me from '../assets/me.jpeg';
 const SplitScreen = () => {
 	return (
 		<div>
-			<div id="about" className="flex flex-col lg:flex-row mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+			<div id="about" className="flex flex-col lg:flex-row mx-auto max-w-7xl px-2 sm:px-6 lg:px-8  bg-white dark:bg-black">
 				{/* Left Half - Content Area */}
-				<div className="w-full lg:w-2/3 bg-white dark:bg-gray-800 flex items-center justify-center">
+				<div className="w-full lg:w-2/3 bg-white dark:bg-black flex items-center justify-center">
 					<div className="text-center py-16">
 						<h1 className="text-4xl font-bold text-gray-900 dark:text-white">Welcome</h1>
 						<p className="mt-4 text-lg text-gray-600 dark:text-gray-300">Nice to meet you!</p>
